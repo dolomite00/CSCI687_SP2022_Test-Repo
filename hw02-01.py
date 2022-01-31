bf6d2200a3d4c9ef216114254418981fa557a50b
Joon S. Lee <jlee@vsu.edu>
2022-01-30 23:20:22 -0500

    added HW due date

--- hw02-01.py
+++ hw02-01.py
@@ -1,6 +1,7 @@
 """
 Virginia State University
 CSCI687: HW02-01
+Due: 2/7/2022
 
 Write a program that takes the length of an edge (an integer) as input and prints the cube's 
 surface area as output. An example of the program input and output is shown below:
