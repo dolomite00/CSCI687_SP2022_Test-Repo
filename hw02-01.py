8caecbcbef74af5589cd463bb3c9ab153d2b2d05
Joon S. Lee <jlee@vsu.edu>
2022-01-30 23:19:06 -0500

    added HW02-01.py

---
+++ hw02-01.py
@@ -0,0 +1,11 @@
+"""
+Virginia State University
+CSCI687: HW02-01
+
+Write a program that takes the length of an edge (an integer) as input and prints the cube's 
+surface area as output. An example of the program input and output is shown below:
+
+        Enter the cube's edge: 4
+        The surface area is 96 square units.
+        
+"""
